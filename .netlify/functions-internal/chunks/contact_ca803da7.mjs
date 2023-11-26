21817cf2b7bd174e28e0f2a907e5e998f76ae8a2
PromaDog <eoe0929808910@gmail.com>
2023-11-27 00:48:07 +0800

    final-1

---
+++ .netlify/functions-internal/chunks/contact_ca803da7.mjs
@@ -0,0 +1,6 @@
+export { renderers } from '../renderers.mjs';
+export { onRequest } from '../_empty-middleware.mjs';
+
+const page = () => import('./pages/contact_8e0394f1.mjs');
+
+export { page };
--- .netlify/functions-internal/entry.mjs
+++ .netlify/functions-internal/entry.mjs
@@ -1,6 +1,6 @@
 import * as adapter from '@astrojs/netlify/netlify-functions.js';
 import { renderers } from './renderers.mjs';
-import { manifest } from './manifest_2266d742.mjs';
+import { manifest } from './manifest_8172657d.mjs';
 
 const _page0  = () => import('./chunks/generic_63a2367f.mjs');
 const _page1  = () => import('./chunks/index_6faa785b.mjs');
@@ -8,7 +8,7 @@ const _page2  = () => import('./chunks/inukami-index_07406611.mjs');
 const _page3  = () => import('./chunks/menu-fried_49b19fa6.mjs');
 const _page4  = () => import('./chunks/menu-ramen_4f605e4c.mjs');
 const _page5  = () => import('./chunks/menu-niku_228e7d31.mjs');
-const _page6  = () => import('./chunks/contact_fa7a23ad.mjs');const pageMap = new Map([["node_modules/astro/dist/assets/endpoint/generic.js", _page0],["src/pages/index.astro", _page1],["src/pages/inu-kami/inukami-index.astro", _page2],["src/pages/inu-kami/menu-fried.astro", _page3],["src/pages/inu-kami/menu-ramen.astro", _page4],["src/pages/inu-kami/menu-niku.astro", _page5],["src/pages/inu-kami/contact.astro", _page6]]);
+const _page6  = () => import('./chunks/contact_ca803da7.mjs');const pageMap = new Map([["node_modules/astro/dist/assets/endpoint/generic.js", _page0],["src/pages/index.astro", _page1],["src/pages/inu-kami/inukami-index.astro", _page2],["src/pages/inu-kami/menu-fried.astro", _page3],["src/pages/inu-kami/menu-ramen.astro", _page4],["src/pages/inu-kami/menu-niku.astro", _page5],["src/pages/inu-kami/contact.astro", _page6]]);
 const _manifest = Object.assign(manifest, {
 	pageMap,
 	renderers,
